4dd39d6690d6102f03704292dd13dd312c10f8b7
devopati <davidopati70@gmail.com>
2024-04-01 23:01:24 +0300

    added root navigation

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { enableFreeze } from "react-native-screens";
+import * as Splashscreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import RootNavigation from "./src/navigation/RootNavigation";
+import { useCallback } from "react";
+import { View } from "react-native";
+
+enableFreeze(true);
+
+Splashscreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, fontsError] = useFonts({
+    orbitronLight: require("./assets/fonts/Orbitron-Black.ttf"),
+    orbitronRegular: require("./assets/fonts/Orbitron-Regular.ttf"),
+    orbitronMedium: require("./assets/fonts/Orbitron-Medium.ttf"),
+    orbitronSemiBold: require("./assets/fonts/Orbitron-SemiBold.ttf"),
+    orbitronBold: require("./assets/fonts/Orbitron-Bold.ttf"),
+  });
+
+  const handleOnLayout = useCallback(async () => {
+    if (fontsLoaded) {
+      await Splashscreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View onLayout={handleOnLayout} style={{ flex: 1 }}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <BottomSheetModalProvider>
+          <StatusBar style="auto" />
+          <RootNavigation />
+        </BottomSheetModalProvider>
+      </GestureHandlerRootView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
