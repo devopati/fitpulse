2c36aaa809e423628b85fb3c4e14ffc385469626
devopati <davidopati70@gmail.com>
2024-04-01 23:02:56 +0300

    route names

---
+++ src/constants/route-names.js
@@ -0,0 +1,13 @@
+export const AUTH_STACK = "auth-stack";
+export const INTRO_SCREEN = "intro-screen";
+export const REGISTER_SCREEN = "register-screen";
+export const LOGIN_SCREEN = "login-screen";
+
+export const BOTTOM_TABS = "bottom-tabs";
+export const DISCOVER_SCREEN = "discover-screen";
+export const WORKOUT_PLAN_SCREEN = "work-out-plan";
+export const STATISTICS_SCREEN = "statistics-screen";
+export const PROFILE_SCREEN = "profile-screen";
+
+export const STACK_SCREENS = "stack-screens";
+export const USER_INFO_SCREEN = "user-info-screen";
