96bd0e43f8d6ebc7b1c9d1af6abb371c969e790d
devopati <davidopati70@gmail.com>
2024-04-02 10:15:12 +0300

    added content

--- src/screens/auth/Intro.js
+++ src/screens/auth/Intro.js
@@ -1,17 +1,42 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { containerStyle } from "../../custom/custom-styles";
 import HeadingText from "../../components/texts/HeadingText";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../../assets/images";
+import IntroBtn from "./components/IntroBtn";
+import Seperator from "./components/Seperator";
 
 const Intro = () => {
   return (
-    <View style={[styles.container, containerStyle]}>
-      <View>
-        <HeadingText>FITPULSE</HeadingText>
-      </View>
+    <ImageBackground source={images.intro} style={styles.container}>
+      <SafeAreaView
+        style={[
+          {
+            flex: 1,
+            paddingTop: 55,
+            backgroundColor: "rgba(56, 87, 255, 0.14)",
+          },
+          containerStyle,
+        ]}
+      >
+        <View>
+          <HeadingText textStyle={styles.title}>FITPULSE</HeadingText>
+        </View>
 
-      <View></View>
-    </View>
+        <View style={styles.bottomView}>
+          <View>
+            <HeadingText textStyle={styles.text}>
+              YOUR ULTIMATE FITNESS COMPANION
+            </HeadingText>
+
+            <Seperator />
+          </View>
+
+          <IntroBtn />
+        </View>
+      </SafeAreaView>
+    </ImageBackground>
   );
 };
 
@@ -20,6 +45,20 @@ export default Intro;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 44,
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 38,
+    fontFamily: "orbitronBold",
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 25,
+  },
+  bottomView: {
+    position: "absolute",
+    alignSelf: "center",
+    bottom: 50,
+    gap: 200,
   },
 });
