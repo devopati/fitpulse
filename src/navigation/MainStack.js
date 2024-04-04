b01ee029efff6ed33f8c9d3ef61341efa32f170a
devopati <davidopati70@gmail.com>
2024-04-04 11:21:09 +0300

    state for get started

--- src/navigation/MainStack.js
+++ src/navigation/MainStack.js
@@ -3,14 +3,19 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { BOTTOM_TABS, STACK_SCREENS } from "../constants/route-names";
 import BottomTabs from "./BottomTabs";
 import StackedNavigation from "./StackedNavigation";
+import { useSelector } from "react-redux";
 
 const Stack = createNativeStackNavigator();
 
 const MainStack = () => {
+  const { showGetStarted } = useSelector((state) => state.auth);
   return (
-    <Stack.Navigator>
-      <Stack.Screen name={BOTTOM_TABS} component={BottomTabs} />
-      <Stack.Screen name={STACK_SCREENS} component={StackedNavigation} />
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {showGetStarted ? (
+        <Stack.Screen name={STACK_SCREENS} component={StackedNavigation} />
+      ) : (
+        <Stack.Screen name={BOTTOM_TABS} component={BottomTabs} />
+      )}
     </Stack.Navigator>
   );
 };
