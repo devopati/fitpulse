62f4911076f2c4404e34718f4b8dc4784d3482ad
devopati <davidopati70@gmail.com>
2024-04-04 11:21:24 +0300

    isloggedin state

--- src/navigation/RootNavigation.js
+++ src/navigation/RootNavigation.js
@@ -2,9 +2,10 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import MainStack from "./MainStack";
 import AuthStack from "./AuthStack";
+import { useSelector } from "react-redux";
 
 const RootNavigation = () => {
-  const isLoggedIn = false;
+  const { isLoggedIn } = useSelector((state) => state.auth);
 
   return (
     <NavigationContainer>
