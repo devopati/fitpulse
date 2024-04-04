c2446ce5fe502a27c092755dc6588730b3941393
devopati <davidopati70@gmail.com>
2024-04-04 11:18:18 +0300

    modified icon reference

---
+++ assets/icons/index.js
@@ -0,0 +1,11 @@
+export const icons = {
+  plan: require("../icons/plan.png"),
+  discover: require("../icons/dicover.png"),
+  stats: require("../icons/stats.png"),
+  profile: require("../icons/profile.png"),
+  muscle: require("../icons/muscle_11188327.png"),
+  settings: require("../icons/setting_2040504.png"),
+  edit: require("../icons/pen_12080750.png"),
+  logout: require("../icons/exit_1286853.png"),
+  fire: require("../icons/fire.png"),
+};
