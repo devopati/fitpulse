fad4fc51aa66b51bdeeff6e52855beaab8482afe
devopati <davidopati70@gmail.com>
2024-04-05 19:25:04 +0300

    render url

--- src/constants/backend-url.js
+++ src/constants/backend-url.js
@@ -1,1 +1,2 @@
-export const BACKEND_URL = "http://192.168.158.3:5000/api/v1";
+// export const BACKEND_URL = "http://192.168.158.3:5000/api/v1";
+export const BACKEND_URL = "https://fitpulse-l2ad.onrender.com/api/v1";
